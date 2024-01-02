70510272fe05dbb05e66b59e08f014f556be13ea
mikastiv <mikastiv@outlook.com>
2024-01-02 16:26:54 -0500

    pass 3st stage

--- app/main.zig
+++ app/main.zig
@@ -20,33 +20,78 @@ pub fn main() !void {
             std.os.exit(1);
         };
         var string = std.ArrayList(u8).init(allocator);
-        switch (decoded) {
-            .string => |str| try std.json.stringify(str, .{}, string.writer()),
-            .int => |i| _ = try string.writer().write(i),
-        }
+        try writeDecoded(decoded, string.writer());
         const jsonStr = try string.toOwnedSlice();
         try stdout.print("{s}\n", .{jsonStr});
     }
 }
 
-const DecodeResult = union(enum) {
+fn writeDecoded(value: Decoded, writer: std.ArrayList(u8).Writer) !void {
+    switch (value) {
+        .string => |str| try std.json.stringify(str, .{}, writer),
+        .int => |i| _ = try writer.writeAll(i),
+        .list => |list| {
+            try writer.writeByte('[');
+            for (list, 0..) |item, i| {
+                try writeDecoded(item, writer);
+                if (i != list.len - 1) try writer.writeByte(',');
+            }
+            try writer.writeByte(']');
+        },
+    }
+}
+
+const Decoded = union(enum) {
     string: []const u8,
     int: []const u8,
+    list: []Decoded,
+
+    fn len(self: @This()) usize {
+        return switch (self) {
+            .string => |str| str.len + countDigits(str.len) + 1,
+            .int => |i| i.len + 2,
+            .list => 0,
+        };
+    }
 };
 
-fn decodeBencode(encodedValue: []const u8) !DecodeResult {
+fn countDigits(n: usize) usize {
+    var count: usize = 1;
+    var num = n;
+    while (num > 9) : (count += 1) {
+        num /= 10;
+    }
+    return count;
+}
+
+fn decodeBencode(encodedValue: []const u8) !Decoded {
     switch (encodedValue[0]) {
         '0'...'9' => {
-            const firstColon = std.mem.indexOf(u8, encodedValue, ":");
-            if (firstColon == null) {
-                return error.InvalidArgument;
-            }
-            return .{ .string = encodedValue[firstColon.? + 1 ..] };
+            const firstColon = std.mem.indexOfScalar(u8, encodedValue, ':');
+            if (firstColon == null) return error.InvalidArgument;
+
+            const len = try std.fmt.parseInt(usize, encodedValue[0..firstColon.?], 10);
+            const start = firstColon.? + 1;
+            return .{ .string = encodedValue[start .. start + len] };
         },
         'i' => {
             const end = std.mem.indexOfScalar(u8, encodedValue, 'e');
-            if (encodedValue.len < 1 or encodedValue[encodedValue.len - 1] != 'e') return error.InvalidArgument;
+            if (end == null) return error.InvalidArgument;
+
             return .{ .int = encodedValue[1..end.?] };
+        },
+        'l' => {
+            const end = std.mem.lastIndexOfScalar(u8, encodedValue, 'e');
+            if (end == null) return error.InvalidArgument;
+
+            var list = std.ArrayList(Decoded).init(allocator);
+            var current: usize = 1;
+            while (current < end.?) {
+                const next = try decodeBencode(encodedValue[current..end.?]);
+                try list.append(next);
+                current += next.len();
+            }
+            return .{ .list = try list.toOwnedSlice() };
         },
         else => {
             try stdout.print("Only strings are supported at the moment\n", .{});
