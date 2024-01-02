da1abc7a66ad41a1dd52d14b19ca9fd6d3259ff5
mikastiv <mikastiv@outlook.com>
2024-01-02 15:36:04 -0500

    pass 2nd stage

--- app/main.zig
+++ app/main.zig
@@ -15,30 +15,38 @@ pub fn main() !void {
 
     if (std.mem.eql(u8, command, "decode")) {
         const encodedStr = args[2];
-        const decodedStr = decodeBencode(encodedStr) catch {
+        const decoded = decodeBencode(encodedStr) catch {
             try stdout.print("Invalid encoded value\n", .{});
             std.os.exit(1);
         };
         var string = std.ArrayList(u8).init(allocator);
-        try std.json.stringify(decodedStr, .{}, string.writer());
+        switch (decoded) {
+            .string => |str| try std.json.stringify(str, .{}, string.writer()),
+            .int => |i| _ = try string.writer().write(i),
+        }
         const jsonStr = try string.toOwnedSlice();
         try stdout.print("{s}\n", .{jsonStr});
     }
 }
 
-fn decodeBencode(encodedValue: []const u8) ![]const u8 {
+const DecodeResult = union(enum) {
+    string: []const u8,
+    int: []const u8,
+};
+
+fn decodeBencode(encodedValue: []const u8) !DecodeResult {
     switch (encodedValue[0]) {
         '0'...'9' => {
             const firstColon = std.mem.indexOf(u8, encodedValue, ":");
             if (firstColon == null) {
                 return error.InvalidArgument;
             }
-            return encodedValue[firstColon.? + 1 ..];
+            return .{ .string = encodedValue[firstColon.? + 1 ..] };
         },
         'i' => {
             const end = std.mem.indexOfScalar(u8, encodedValue, 'e');
             if (encodedValue.len < 1 or encodedValue[encodedValue.len - 1] != 'e') return error.InvalidArgument;
-            return encodedValue[1..end.?];
+            return .{ .int = encodedValue[1..end.?] };
         },
         else => {
             try stdout.print("Only strings are supported at the moment\n", .{});
